c2cc62fdc1d1f714a81cdb2787bccaa91ca0db15
marshalyordanos <marshalyordanosseifu@gmail.com>
2022-05-01 18:33:41 +0300

    chanege

---
+++ backend/data/Products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    name: "AirPods wireless Bluetooth Headphones",
+    image: "/images/image1.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Amazon Alexa",
+    image: "/images/image2.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Apple watch",
+    image: "/images/image7.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "HeadSet",
+    image: "/images/image4.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Bag",
+    image: "/images/image5.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Tv Samsung 55",
+    image: "/images/image6.jpg",
+    description:
+      "Bluetooth rechnology lets you connect \
+        it with compatible devices wirelessly high-quality AAC audi offers immersuve listening exprience \
+        Built-in microphone allows you to take calls while working,  ",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    cuntInStoke: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+];
+
+export default products;
